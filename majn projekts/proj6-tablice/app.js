0fdd563301b15f3c7c37a57b9bc179789c8613a0
apokalipsa44 <m_lachowicz@wp.pl>
2019-11-02 14:00:13 +0100

    user sort

--- majn projekts/proj6-tablice/app.js
+++ majn projekts/proj6-tablice/app.js
@@ -3,17 +3,26 @@ const data = {
         {
             id: 1,
             name: "judek",
-            age: 15
+            age: 15,
+            sex: "male"
         },
         {
             id: 2,
             name: "kuba",
-            age: 33
+            age: 33,
+            sex: "male"
         },
         {
             id: 3,
             name: "sterfan",
-            age: 34
+            age: 34,
+            sex: "female"
+        },
+        {
+            id: 4,
+            name: "sterfania",
+            age: 44,
+            sex: "female"
         }
     ]
 }
@@ -22,20 +31,53 @@ const Item = ({user}) => (
     <div>
         <h1>user name is {user.name}</h1>
         <h2>age is {user.age}</h2>
+        <h2>user sex is <strong>{user.sex}</strong></h2>
     </div>
 )
 
 
 class App extends React.Component {
-    state = {}
+    state = {
+        select: "all"
+    }
 
+    usersList = () => {
+        let users = this.props.data.users
+        switch (this.state.select) {
+            case "all": {
+                users = users.map(user => <Item key={user.id} user={user}/>)
+                return users
+            }
+            case "men": {
+                users = users.filter((user) => user.sex === "male")
+                users = users.map(user => <Item key={user.id} user={user}/>)
+                return users
+            }
+            case "women": {
+                users = users.filter((user) => user.sex === "female")
+                users = users.map(user => <Item key={user.id} user={user}/>)
+                return users
+            }
+        }
+    }
+
+    handleUsersFilter(option) {
+        this.setState({
+            select: option
+        })
+    }
 
     render() {
-        const users = this.props.data.users
-        const Items = users.map(user => <Item key={user.id} user={user}/>)
+        // let users = this.props.data.users
+        // // filtruje na mniejszą tablicę i ją wyświetla po Items
+        // const Items = users.map(user => <Item key={user.id} user={user}/>)
         return (
             <>
-                {Items}
+                <button onClick={this.handleUsersFilter.bind(this, "all")}>All</button>
+                <button onClick={this.handleUsersFilter.bind(this, "women")}>Women</button>
+                <button onClick={this.handleUsersFilter.bind(this, "men")}>Men</button>
+                {this.usersList()}
+
             </>
         );
     }
--- rozw/rozdz2-rozwiazania/11-ListItems-v3/app.js
+++ rozw/rozdz2-rozwiazania/11-ListItems-v3/app.js
@@ -4,21 +4,25 @@ const data = {
       id: 1,
       age: 29,
       name: "Arek",
+      sex: "male"
     },
     {
       id: 2,
       age: 49,
       name: "Marta",
+      sex:"female"
     },
     {
       id: 3,
       age: 19,
       name: "Stasia",
+      sex: "female"
     },
     {
       id: 4,
       age: 24,
       name: "Karol",
+      sex: "male"
     }
   ]
 }
@@ -27,6 +31,7 @@ const Item = ({ user }) => (
   <div>
     <h1>Użytkownik {user.name}</h1>
     <h2>Ma {user.age}</h2>
+    <h2>Płeć użytkownika {user.sex}</h2>
   </div>
 )
 class ListItems extends React.Component {
