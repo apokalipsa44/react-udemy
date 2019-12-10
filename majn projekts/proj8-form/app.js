2891ff4b02e1f872f7acbdaf76bfeb5ba75a652d
apokalipsa44 <m_lachowicz@wp.pl>
2019-12-11 00:41:07 +0100

    proj 8

--- majn projekts/proj8-form/app.js
+++ majn projekts/proj8-form/app.js
@@ -1,48 +1,66 @@
 class Form extends React.Component {
     state = {
         city: "marakesz",
-        text:"",
+        text: "",
         isLoved: true,
         visits: 2
     }
 
-    handleCityChange = (e) => {
-        this.setState({
-            city: e.target.value
-        })
-    }
-    handleTextChange = (e) => {
-        this.setState({
-            text: e.target.value
-        })
-    }
-    handleIsLovedChange = (e) => {
-        this.setState({
-            isLoved: e.target.checked
-        })
-    }
+    // handleCityChange = (e) => {
+    //     this.setState({
+    //         city: e.target.value
+    //     })
+    // }
+    // handleTextChange = (e) => {
+    //     this.setState({
+    //         text: e.target.value
+    //     })
+    // }
+    // handleIsLovedChange = (e) => {
+    //     this.setState({
+    //         isLoved: e.target.checked
+    //     })
+    // }
+    //
+    // handleVisitsNumberChange=(e)=>{
+    //     this.setState({
+    //         visits:e.target.value
+    //     })
+    // }
 
-    handleVisitsNumberChange=(e)=>{
-        this.setState({
-            visits:e.target.value
-        })
+    // albo jedna metooda
+
+    handleChange = (e) => {
+        console.log(e.target.type);
+        if (e.target.type === "checkbox") {
+            this.setState({
+                [e.target.name]: e.target.checked
+            })
+        } else {
+
+            this.setState({
+                [e.target.name]: e.target.value
+            })
+        }
     }
 
     render() {
         return (
             <>
-                <label>podaj city: <input type="text" value={this.state.city} name="city" onChange={this.handleCityChange}/></label>
+                <label>podaj city: <input name="city" type="text" value={this.state.city} name="city"
+                                          onChange={this.handleChange}/></label>
                 <br/>
-                <label>podaj email: <textarea type="text" value={this.state.text} onChange={this.handleTextChange}/></label>
+                <label>podaj email: <textarea name="text" type="text" value={this.state.text}
+                                              onChange={this.handleChange}/></label>
                 <br/>
-                <label>Kumasz? <input type="checkbox" checked={this.state.isLoved}
-                                      onChange={this.handleIsLovedChange}/></label>
+                <label>Kumasz? <input name="isLoved" type="checkbox" checked={this.state.isLoved}
+                                      onChange={this.handleChange}/></label>
                 <br/><label>Ile wizyt? </label>
-                <select value={this.state.visits} onChange={this.handleVisitsNumberChange}>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="10">10</option>
-                <option value="more">more</option>
+                <select name="visits" value={this.state.visits} onChange={this.handleChange}>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="10">10</option>
+                    <option value="more">more</option>
                 </select>
                 <br/>
                 <button>Save</button>
