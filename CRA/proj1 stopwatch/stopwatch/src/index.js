667d4d89ffcbd8b854373cee93bcd5a5f9b89319
apokalipsa44 <m_lachowicz@wp.pl>
2019-12-19 12:51:58 +0100

    validated form

--- CRA/proj1 stopwatch/stopwatch/src/index.js
+++ CRA/proj1 stopwatch/stopwatch/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './fetch-users/App';
+import App from './validated-form/App';
 // import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
---
+++ CRA/proj1 stopwatch/stopwatch/src/validated-form/App.js
@@ -0,0 +1,47 @@
+import React, {Component} from "react";
+import "./App.css"
+
+
+class App extends Component {
+    state = {
+        username: "",
+        email: "",
+        password: "",
+        checked: ""
+    }
+
+
+    render() {
+
+        return (
+            <>
+                <form>
+                    <label htmlFor="user">Enter username
+                        <input type="text" id="user" name="username" value={this.state.username} onChange={this.handleOnChange}/>
+                    </label>
+                    <label htmlFor="email">Enter email
+                        <input type="email" id="email" name="email" value={this.state.email} onChange={this.handleOnChange}/>
+                    </label>
+                    <label htmlFor="password">Enter password
+                        <input type="password" id="password" name="password" value={this.state.password} onChange={this.handleOnChange}/>
+                    </label>
+
+
+                </form>
+
+            </>
+        )
+    }
+
+    handleOnChange() {
+        
+this.setState({
+
+})
+    }
+}
+
+export default App;
+
+
+
