acd2557b2711df79d7b203c5434b64878da912d1
apokalipsa44 <m_lachowicz@wp.pl>
2019-12-21 13:27:28 +0100

    images in header

--- CRA/react-sample/src/layouts/Header.js
+++ CRA/react-sample/src/layouts/Header.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom'
+import bmw1 from '../images/e361.jpg'
+import bmw2 from '../images/e362.jpg'
+import bmw3 from '../images/e363.jpg'
+import bmw4 from '../images/e364.jpg'
+import '../styles/Header.css'
 
 const Header = () => {
     return (
-        <div>
-            header
-        </div>
+
+        <Switch>
+                <Route path="/" exact="true" render={() => (
+                    <img src={bmw1} alt="bmw"/>
+                )}/>
+                <Route path="/products" exact="true" render={() => (
+                    <img src={bmw2} alt="bmw"/>
+                )}/>
+                <Route path="/contact" exact="true" render={() => (
+                    <img src={bmw3} alt="bmw"/>
+                )}/>
+                <Route path="/admin" exact="true" render={() => (
+                    <img src={bmw4} alt="bmw"/>
+                )}/>
+                <Route render={()=>(
+                    <img src={bmw4} alt="bmw"/>
+                )}/>
+        </Switch>
     );
 };
 
