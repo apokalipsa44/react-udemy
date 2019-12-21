dbb3016b2e3f4ebb41071d0fb83b79bd6593acdd
apokalipsa44 <m_lachowicz@wp.pl>
2019-12-21 13:48:29 +0100

    main viewrouting

---
+++ CRA/react-sample/src/components/Products.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Products = () => {
+    return (
+        <div>
+            products component
+        </div>
+    );
+};
+
+export default Products;
--- CRA/react-sample/src/layouts/Page.js
+++ CRA/react-sample/src/layouts/Page.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import {Route, Switch} from "react-router-dom";
+import HomePage from "../components/HomePage"
+import Products from "../components/Products"
+import Contact from "../components/Contact"
+import Admin from "../components/Admin"
+import ErrorPage from "../components/ErrorPage";
 
 const Page = () => {
+
     return (
-        <div>
-            page
-        </div>
+        <Switch>
+            <Route path="/" exact component={HomePage}/>
+            <Route path="/products" exact component={Products}/>
+            <Route path="/contact" exact component={Contact}/>
+            <Route path="/admin" exact component={Admin}/>
+            <Route component={ErrorPage}/>
+        </Switch>
     );
 };
 
