cfdff5d95d1510975d75dba8790d0fd1a48ab4d9
apokalipsa44 <m_lachowicz@wp.pl>
2019-12-21 12:55:04 +0100

    nav done and styled

--- CRA/react-sample/src/layouts/Navigation.js
+++ CRA/react-sample/src/layouts/Navigation.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import {NavLink} from "react-router-dom";
+import "../styles/Navigation.css"
+
+
+const menuList = [
+    {name: "home", path: "/"},
+    {name: "products", path: "/products"},
+    {name: "contact", path: "/contact"},
+    {name: "admin", path: "/admin"}
+]
 
 const Navigation = () => {
+
+    const menu = menuList.map(menu => (
+        <li key={menu.name}>
+            <NavLink to={menu.path} exact="true">{menu.name}</NavLink>
+        </li>
+    ))
+
     return (
         <div>
-            nav
+            <nav>
+                <ul>
+                    {menu}
+                </ul>
+
+
+            </nav>
         </div>
     );
 };
